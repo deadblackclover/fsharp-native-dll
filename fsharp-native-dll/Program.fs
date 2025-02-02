1e44eff4d6fe519d28ec3bdb261d38ba616e395b
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2025-02-02 17:41:07 +0700

    Added native_zig

--- fsharp-native-dll/Program.fs
+++ fsharp-native-dll/Program.fs
@@ -7,11 +7,17 @@ module NativeRust =
     [<DllImport(@"..\..\..\..\native_rust\target\debug\native_rust", EntryPoint = "magic_function")>]
     extern int MagicFunction(int input)
 
+module NativeZig =
+    [<DllImport(@"..\..\..\..\native_zig\zig-out\bin\native_zig", EntryPoint = "hello")>]
+    extern void Hello()
+
 [<EntryPoint>]
 let main _ =
     NativeRust.Hello()
 
     let result = NativeRust.MagicFunction(42)
     printfn "Result: %d" result
+
+    NativeZig.Hello()
 
     0
---
+++ native_zig/src/root.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+export fn hello() void {
+    std.debug.print("Hello from native_zig.dll", .{});
+}
