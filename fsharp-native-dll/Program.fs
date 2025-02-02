3d656013ef2e4c9ae5b52ba12673568210c475ec
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2025-02-02 13:53:30 +0700

    Add project files.

---
+++ fsharp-native-dll/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.Runtime.InteropServices
+
+module NativeRust =
+    [<DllImport(@"..\..\..\..\native_rust\target\debug\native_rust", EntryPoint = "hello")>]
+    extern void Hello()
+
+    [<DllImport(@"..\..\..\..\native_rust\target\debug\native_rust", EntryPoint = "magic_function")>]
+    extern int MagicFunction(int input)
+
+[<EntryPoint>]
+let main _ =
+    NativeRust.Hello()
+
+    let result = NativeRust.MagicFunction(42)
+    printfn "Result: %d" result
+
+    0
